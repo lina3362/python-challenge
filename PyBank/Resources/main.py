bd88b2e6de632e24d1d7557dd03a981992dc6af8
Lina Ma <lina.ma0616@gmail.com>
2020-10-04 20:53:46 -0600

    4

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -2,23 +2,30 @@
 import os
 import csv
 path = os.path.join("..","Resources","budget_data.csv")
-
+#variables
+month_count = 0
+totalrev = 0
+differences = 0
+averagechange = 0
+Highest_profit = 0
+Lowest_profit = 0
+# read the csv file
 with open(path) as file:
     csvreader = csv.reader(file, delimiter=",")
+    header = next(csvreader)
+    for row in csvreader:
   # The total number of months included in the dataset
-    row_count = sum(1 for row in file) -1
+      month_count += 1
   # The net total amount of "Profit/Losses" over the entire period
-    Total = 0
-    totalrev = Total + int(row["Profit/Losses"])
+      totalrev += int(row[1])
   # The average of the changes in "Profit/Losses" over the entire period
-
   # The greatest increase in profits (date and amount) over the entire period
 
   # The greatest decrease in losses (date and amount) over the entire period
 
 print("Financial Analysis")
 print("-----------------------")
-print("Total Months:", row_count)
+print("Total Months:", month_count)
 print("Total:", totalrev)
 
 
